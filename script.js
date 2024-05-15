eb84c6adb8d168c4a79cc6cf7431e139ed364911
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-15 08:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+    if(s==null || s.length==0)
+        return s;
+    else{
+        let ans="";
+        for(let i=0;i<s.length;i++){
+            if(s[i]==' '){
+                break;
+            }
+            else
+                ans+=s[i];
+        }
+        return ans;
+    }
 }
 
 // Do not change the code below
